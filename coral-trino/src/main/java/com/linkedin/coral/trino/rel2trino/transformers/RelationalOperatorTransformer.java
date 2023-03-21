c56990acb0111753646f045e1128ce3f41f00a6e
Jiefan Li <jiefli@linkedin.com>
2023-03-21 14:52:56 -0400

    test

--- coral-trino/src/main/java/com/linkedin/coral/trino/rel2trino/transformers/RelationalOperatorTransformer.java
+++ coral-trino/src/main/java/com/linkedin/coral/trino/rel2trino/transformers/RelationalOperatorTransformer.java
@@ -5,6 +5,7 @@
  */
 package com.linkedin.coral.trino.rel2trino.transformers;
 
+import java.util.List;
 import java.util.Set;
 
 import org.apache.calcite.rel.type.RelDataType;
@@ -18,26 +19,31 @@ import org.apache.calcite.sql.SqlTypeNameSpec;
 import org.apache.calcite.sql.type.SqlTypeFamily;
 import org.apache.calcite.sql.validate.SqlValidator;
 
+import com.linkedin.coral.com.google.common.collect.ImmutableList;
 import com.linkedin.coral.com.google.common.collect.ImmutableMultimap;
 import com.linkedin.coral.com.google.common.collect.ImmutableSet;
 import com.linkedin.coral.com.google.common.collect.Multimap;
 import com.linkedin.coral.common.transformers.SqlCallTransformer;
 import com.linkedin.coral.trino.rel2trino.TrinoTryCastFunction;
 
+import static org.apache.calcite.sql.SqlKind.*;
 import static org.apache.calcite.sql.parser.SqlParserPos.*;
+import static org.apache.calcite.sql.type.SqlTypeFamily.*;
 
 
+/**
+ * Coral IR allows implicit castings like casting from int to varchar for relational operators. However, Trino requires explicit casting.
+ * This transformer adds explicit {@link TrinoTryCastFunction} to ensure operand inter-compatibility for Trino.
+ *
+ * For example, `"0" = 1` is transformed into `TRY_CAST("0" AS INT) = 1`
+ */
 public class RelationalOperatorTransformer extends SqlCallTransformer {
 
   // SUPPORTED_TYPE_CAST_MAP is a static mapping that maps a SqlTypeFamily key to its set of
   // type-castable SqlTypeFamilies.
-  private static final Multimap<SqlTypeFamily, SqlTypeFamily> SUPPORTED_TYPE_CAST_MAP =
-      ImmutableMultimap.<SqlTypeFamily, SqlTypeFamily> builder()
-          .putAll(SqlTypeFamily.CHARACTER, SqlTypeFamily.BOOLEAN, SqlTypeFamily.NUMERIC, SqlTypeFamily.DATE,
-              SqlTypeFamily.TIME, SqlTypeFamily.TIMESTAMP)
-          .putAll(SqlTypeFamily.NUMERIC, SqlTypeFamily.DATE, SqlTypeFamily.TIME, SqlTypeFamily.TIMESTAMP)
-          .putAll(SqlTypeFamily.BOOLEAN, SqlTypeFamily.NUMERIC)
-          .putAll(SqlTypeFamily.BINARY, SqlTypeFamily.CHARACTER, SqlTypeFamily.NUMERIC).build();
+  private static final Multimap<SqlTypeFamily, SqlTypeFamily> SUPPORTED_TYPE_CAST_MAP = ImmutableMultimap
+      .<SqlTypeFamily, SqlTypeFamily> builder().putAll(CHARACTER, BOOLEAN, NUMERIC, DATE, TIME, TIMESTAMP)
+      .putAll(NUMERIC, DATE, TIME, TIMESTAMP).putAll(BOOLEAN, NUMERIC).putAll(BINARY, CHARACTER, NUMERIC).build();
   private static final Set<SqlKind> RELATIONAL_OPERATOR_SQL_KIND = ImmutableSet.of(SqlKind.EQUALS, SqlKind.GREATER_THAN,
       SqlKind.GREATER_THAN_OR_EQUAL, SqlKind.LESS_THAN, SqlKind.LESS_THAN_OR_EQUAL, SqlKind.NOT_EQUALS);
 
@@ -49,32 +55,37 @@ public class RelationalOperatorTransformer extends SqlCallTransformer {
   protected boolean condition(SqlCall sqlCall) {
     final SqlOperator operator = sqlCall.getOperator();
     if (RELATIONAL_OPERATOR_SQL_KIND.contains(operator.getKind())) {
-      SqlNode leftOperand = sqlCall.operand(0);
+      final SqlNode leftOperand = sqlCall.operand(0);
       final SqlNode rightOperand = sqlCall.operand(1);
-      if (leftOperand.getKind() == SqlKind.CAST) {
-        leftOperand = ((SqlCall) leftOperand).operand(0);
-      }
-      return SUPPORTED_TYPE_CAST_MAP.containsEntry(getRelDataType(leftOperand).getSqlTypeName().getFamily(),
-          getRelDataType(rightOperand).getSqlTypeName().getFamily());
+      return leftOperand.getKind() != CAST && rightOperand.getKind() != CAST;
     }
     return false;
   }
 
   @Override
   protected SqlCall transform(SqlCall sqlCall) {
-    SqlNode leftOperand = sqlCall.operand(0);
+    final SqlNode leftOperand = sqlCall.operand(0);
     final SqlNode rightOperand = sqlCall.operand(1);
-    if (leftOperand.getKind() == SqlKind.CAST) {
-      leftOperand = ((SqlCall) leftOperand).operand(0);
-    }
 
-    final SqlCall tryCastNode = TrinoTryCastFunction.INSTANCE.createCall(ZERO, leftOperand,
-        getSqlDataTypeSpecForCasting(getRelDataType(rightOperand)));
-    return sqlCall.getOperator().createCall(ZERO, tryCastNode, rightOperand);
+    final RelDataType leftRelDataType = getRelDataType(leftOperand);
+    final RelDataType rightRelDataType = getRelDataType(rightOperand);
+
+    final SqlTypeFamily leftSqlTypeFamily = leftRelDataType.getSqlTypeName().getFamily();
+    final SqlTypeFamily rightSqlTypeFamily = rightRelDataType.getSqlTypeName().getFamily();
+
+    List<SqlNode> updatedOperands = sqlCall.getOperandList();
+
+    if (SUPPORTED_TYPE_CAST_MAP.containsEntry(leftSqlTypeFamily, rightSqlTypeFamily)) {
+      updatedOperands = ImmutableList.of(tryCastOperandToType(leftOperand, rightRelDataType), rightOperand);
+    } else if (SUPPORTED_TYPE_CAST_MAP.containsEntry(rightSqlTypeFamily, leftSqlTypeFamily)) {
+      updatedOperands = ImmutableList.of(leftOperand, tryCastOperandToType(rightOperand, leftRelDataType));
+    }
+    return sqlCall.getOperator().createCall(ZERO, updatedOperands);
   }
 
-  private SqlDataTypeSpec getSqlDataTypeSpecForCasting(RelDataType relDataType) {
+  private SqlCall tryCastOperandToType(SqlNode operand, RelDataType relDataType) {
     final SqlTypeNameSpec typeNameSpec = new SqlBasicTypeNameSpec(relDataType.getSqlTypeName(), ZERO);
-    return new SqlDataTypeSpec(typeNameSpec, ZERO);
+    final SqlDataTypeSpec sqlDataTypeSpec = new SqlDataTypeSpec(typeNameSpec, ZERO);
+    return TrinoTryCastFunction.INSTANCE.createCall(ZERO, operand, sqlDataTypeSpec);
   }
 }
